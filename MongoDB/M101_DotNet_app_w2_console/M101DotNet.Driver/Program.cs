cea5d7291f1757a6325f31ba5cac9032c0de56fc
Sergii Shevchenko (EPAM) <Sergii_Shevchenko@epam.com>
2016-01-21 16:21:55 +0200

    Proj for Mongo .NET Driver added (week 2).

---
+++ MongoDB/M101_DotNet_app_w2_console/M101DotNet.Driver/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace M101DotNet.Driver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+
+            Console.WriteLine();
+            Console.WriteLine("Press Enter");
+            Console.ReadLine();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            var client = new MongoClient();
+        }
+    }
+}
