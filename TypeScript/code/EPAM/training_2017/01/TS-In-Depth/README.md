cf745945f54cc96950d0a9a3e3f61e1f67cd69d2
Sergii Shevchenko (EPAM) <Sergii_Shevchenko@epam.com>
2017-12-26 17:02:31 +0200

    [TS IN-DEPTH 2017] Lesson 2.

--- TypeScript/code/EPAM/training_2017/01/TS-In-Depth/README.md
+++ TypeScript/code/EPAM/training_2017/01/TS-In-Depth/README.md
@@ -1,14 +1,36 @@
-# TypeScript-In-Depth
+# EPAM.TypeScript In-Depth #4 - 2017 (Vitaliy Zhyrytskyy)
 
 1. > npm install -g typescript gulp
 2. > npm install
 3. > gulp
 
 
-// ******************************
-// *     INFO SS
-// *    
-// *    npm install -g tslint
-// * npm install -g tslint
-// * npm install -g tslint
-// ******************************
+**------------------------------------ INFO SS  ------------------------------------------**
+#### HOW TO RUN:
+  - Run _gulp_ directly from VSCode terminal window **!!**
+  - TSLint (https://palantir.github.io/tslint/). Install npm package & VSCOde plug-in:
+    ```sh
+    $ npm install -g tslint
+    >> TSLint rules config is here: \TS-In-Depth\tslint.json
+    ```
+  - aaa
+  - aaaa
+  - aaaaaaaa
+
+// TERMINAL:
+//     npm i lodash
+//     npm i @types/lodash
+
+// import * as _ from "lodash";
+// _.each()
+
+
+#### NOTES:
+- backtick == Template String from ES2015 (`My name: ${obj.name}`)
+- v
+- v
+- v
+- v
+- v
+
+**----------------------------------------------------------------------------------------**
--- TypeScript/code/EPAM/training_2017/01/TS-In-Depth/src/app.ts
+++ TypeScript/code/EPAM/training_2017/01/TS-In-Depth/src/app.ts
@@ -1,17 +1,9 @@
-// ******************************
-// *     INFO SS
-// *    
-// *    npm install -g tslint
-// * npm install -g tslint
-// * npm install -g tslint
-// ******************************
+// TERMINAL:
+//     npm i lodash
+//     npm i @types/lodash
 
-
-
-
-
-
-
+// import * as _ from "lodash";
+// _.each()
 
 showHello("greeting", "TypeScript");
 
@@ -21,27 +13,53 @@ function showHello(divName: string, name: string) {
 }
 
 
-// TASK - 01
-enum Category { JavaScript, CSS, HTML, TypeScript }
+// TASKS
+enum Category { JavaScript, CSS, HTML, TypeScript, Angular2 }
 
+interface DamageLogger {
+    (reason: string): void;
+}
 
+interface Book {
+    id: number;
+    title: string;
+    author: string;
+    available: boolean;
+    category: Category;
+    pages?: number;
+    markDamaged?: DamageLogger;
+}
 
-function getAllBooks() {
-    let books: Array<any> = [
-        { title: 'Refactoring JavaScript', author: 'Evan Burchard', available: true, category: Category.JavaScript},
-        { title: 'JavaScript Testing', author: 'Liang Yuxian Eugene', available: false, category: Category.JavaScript },
-        { title: 'CSS Secrets', author: 'Lea Verou', available: true, category: Category.CSS },
-        { title: 'Mastering JavaScript Object-Oriented Programming', author: 'Andrea Chiarelli', available: true, category: Category.JavaScript }
+interface Person {
+    name: string;
+    email: string;
+}
+
+interface Author extends Person {
+    numBooksPublished: number;
+}
+
+interface Librarian extends Person {
+    department: string;
+    assistCustomer: (custName: string) => void;
+}
+
+function getAllBooks(): Book[] {
+    const books = [
+            { id: 1, title: "Refactoring JavaScript", author: "Evan Burchard", available: true, category: Category.JavaScript},
+            { id: 2, title: "JavaScript Testing", author: "Liang Yuxian Eugene", available: false, category: Category.JavaScript },
+            { id: 3, title: "CSS Secrets", author: "Lea Verou", available: true, category: Category.CSS },
+            { id: 4, title: "Mastering JavaScript Object-Oriented Programming", author: "Andrea Chiarelli", available: true, category: Category.JavaScript }
         ];
 
     return books;
 }
 
-function logFirstAvailable(books: Array<any>): void {
+function logFirstAvailable(books: Array<any> = getAllBooks()): void {
     const numberOfBooks: number = books.length;
     let firstAvailable: string = "";
 
-    for (let currentBook of books) { 
+    for (let currentBook of books) {
         if (currentBook.available) {
             firstAvailable = currentBook.title;
             break;
@@ -49,17 +67,22 @@ function logFirstAvailable(books: Array<any>): void {
     }
 
     console.log(`Total Books: ${numberOfBooks}`);
-    console.log(`firstAvailable: ${firstAvailable}`);
+    console.log(`First Available: ${firstAvailable}`);
 }
 
-
-function getBookTitleByCategory(categoryFilter: Category): Array<string> {
+function getBookTitleByCategory(categoryFilter: Category = Category.JavaScript): Array<string> {
     console.log(`Getting books in category: ${Category[categoryFilter]}`);
 
-    // TODO SS
-    const allBooks = getAllBooks();
+    const allBooks: Array<any> = getAllBooks();
+    const filteredTitles: string[] = [];
 
-    return [""];
+    for (let currentBook of allBooks) {
+        if (currentBook.category === categoryFilter) {
+            filteredTitles.push(currentBook.title);
+        }
+    }
+
+    return filteredTitles;
 }
 
 function logBookTitles(titles: string[]): void {
@@ -68,24 +91,159 @@ function logBookTitles(titles: string[]): void {
     }
 }
 
+function getBookById(id: number): Book | undefined {
+    const allBooks = getAllBooks();
 
-// TERMINAL:
-//     npm i lodash
-//     npm i @types/lodash
+    return allBooks.find(book => book.id === id);
+}
 
-// import * as _ from "lodash";
-// _.each()
+function createCustomerId(name: string, id: number): string {
+    return `${name}${id}`;
+}
+
+function createCustomer(name: string, age?: number, city?: string): void {
+    console.log(`Creating customer ${name}`);
+
+    if (age) {
+        console.log(`Age: ${age}`);
+    }
+
+    if (city) {
+        console.log(`City: ${city}`);
+    }
+}
+
+function checkoutBooks(customer: string, ...bookIds: number[]): string[] {
+    console.log(`Checking out books for ${customer}`);
+
+    const bookTitles: string[] = [];
+    for (let id of bookIds) {
+        let book = getBookById(id);
+        if (book && book.available) {
+            bookTitles.push(book.title);
+        }
+    }
+
+    return bookTitles;
+}
+
+function getTitles(author: string): string[];
+function getTitles(available: boolean): string[];
+function getTitles(property: any): string[] {
+    const allBooks = getAllBooks();
+    const bookTitles: string[] = [];
+
+    if (typeof(property) === "string") {
+        for (let book of allBooks) {
+            if (book.author === property) {
+                bookTitles.push(book.title);
+            }
+        }
+    }
+
+    if (typeof(property) === "boolean") {
+        for (let book of allBooks) {
+            if (book.available === property) {
+                bookTitles.push(book.title);
+            }
+        }
+    }
+
+    return bookTitles;
+}
+
+function printBook(book: Book): void {
+    console.log(`"${book.title}" by ${book.author}`);
+}
+
+class UniversityLibrarian implements Librarian {
+    name: string;
+    email: string;
+    department: string;
+
+    assistCustomer(name: string): void {
+        console.log(`${this.name} is assist ${name}.`);
+    }
+}
+
 
 
 
 // ******************************** CONSOLE **************************************
-//---------------------------------------------------------------------------------
-// // TASK - 01
+// -------------------------------------------------------------------------------
+// TASK - 01
 // console.log(getAllBooks());
 
 // const allBooks = getAllBooks();
 // logFirstAvailable(allBooks);
 
 // TASK - 02
-const javaScriptBooks: Array<string> = getBookTitleByCategory(Category.JavaScript);
-logBookTitles(javaScriptBooks);
+// const javaScriptBooks: Array<string> = getBookTitleByCategory(Category.JavaScript);
+// logBookTitles(javaScriptBooks);
+
+// TASK - 03
+// javaScriptBooks.forEach((val, idx, arr) => console.log(`${++idx} - ${val}`));
+// console.log(getBookById(3));
+
+// TASK - 04
+// let myId = createCustomerId("Ann", 10);
+// console.log(myId);
+// let idGenerator: (a: string, b: number) => string;
+// idGenerator = (name: string, id: number) => `${name}${id}`;
+// idGenerator = createCustomerId;
+// myId = idGenerator("Lisa", 33);
+// console.log(myId);
+
+// TASK - 05
+// createCustomer("John");
+// createCustomer("Boris", 55);
+// createCustomer("Clarasana", 16, "New York");
+// const res = getBookTitleByCategory();
+// console.log(res);
+// logFirstAvailable();
+
+// const myBooks = checkoutBooks("Ann", 1, 2, 4);
+// myBooks.forEach(title => console.log(title));
+
+// TASK - 06
+// const checkedOutBooks = getTitles(false);
+// console.log(checkedOutBooks);
+
+// TASK - 07
+// const myBook: Book = {
+//     id: 5,
+//     title: "Colors, Backgrounds, and Gradients",
+//     author: "Eric A. Meyer",
+//     available: true,
+//     category: Category.CSS,
+//     // year: 2015,
+//     // copies: 3
+//     pages: 200,
+//     markDamaged: (reason: string) => console.log(`Damaged!! - ${reason}`)
+// };
+// printBook(myBook);
+// myBook.markDamaged("missing book cover");
+
+// TASK - 08
+// const logDamage: DamageLogger = (reason: string) => console.log(reason);
+// logDamage("coffee stains");
+
+// TASK - 09
+const favoriteAuthor: Author = {
+    email: "Ann@gmail.com",
+    name: "Ann",
+    numBooksPublished: 3
+};
+// const favoriteLibrarian: Librarian = {
+//     email: "Boris@gmail.com",
+//     name: "Boris",
+//     department: "Classical",
+//     assistCustomer: (name: string) => console.log(`Assist ${name}`)
+// };
+
+// TASK - 10
+const favoriteLibrarian: Librarian = new UniversityLibrarian();
+favoriteLibrarian.name = "Ann";
+favoriteLibrarian.assistCustomer("Boris");
+
+
